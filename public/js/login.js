bdc2525d2c566d31817405e96532e6a49cb1bea8
Anibal Rodriguez <anibal.rodriguez@saniklabs.com>
2016-02-25 21:58:55 -0600

    quitando config en el controlador de login

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,6 @@
 
 var app = angular.module('sosialApp', ['ngMaterial','ngCookies']);
 
-app.config(['$httpProvider', function ($httpProvider) {
-  // Intercept POST requests, convert to standard form encoding
-  $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-  $httpProvider.defaults.transformRequest.unshift(function (data, headersGetter) {
-    var key, result = [];
-
-    if (typeof data === "string")
-      return data;
-
-    for (key in data) {
-      if (data.hasOwnProperty(key))
-        result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
-    }
-    return result.join("&");
-  });
-}]);
 
 app.controller('LoginCtrl', function($scope,$cookies,$q,$http) {
 
