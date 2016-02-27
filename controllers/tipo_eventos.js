b75d6e2ff8b6b41d48d11bfbf80ec61ab847176c
David Yzaguirre <dvdyzag@gmail.com>
2016-02-27 15:30:49 -0600

    Corrección de nombre de variable

--- controllers/tipo_eventos.js
+++ controllers/tipo_eventos.js
@@ -23,28 +23,28 @@ exports.listar = function(request, reply){
   }
 };
 exports.create = function(request, reply){
-  new db.Equipo(request.payload).save(function(err, equipo, numberAffected){
+  new db.TipoEvento(request.payload).save(function(err, tipoEvento, numberAffected){
     if(err){
       return reply({statusCode: 600, error: "Database", message: "Error de Base de datos."});
     }
-    return reply({statusCode: 200, _id: equipo._id});
+    return reply({statusCode: 200, _id: tipoEvento._id});
   });
 }
 exports.save = function(request, reply){
-  var equipo = request.payload;
-  equipo.modified = Date.now();
-  db.Equipo.update({_id:request.params.idEquipo}, {$set: equipo}, function(err, raw){
+  var tipoEvento = request.payload;
+  tipoEvento.modified = Date.now();
+  db.TipoEvento.update({_id:request.params.idTipo}, {$set: tipoEvento}, function(err, raw){
     if (err) {
-      console.log("EQUIPOS_SAVE err="+JSON.stringify(err));
+      console.log("TIPO_EVENTOS_SAVE err="+JSON.stringify(err));
       return reply({statusCode:600});
     }
     return reply({statusCode: 200});
   });
 }
 exports.delete = function(request, reply){
-  db.Equipo.remove({_id:request.params.idEquipo}, function(err){
+  db.TipoEvento.remove({_id:request.params.idTipo}, function(err){
     if (err) {
-      console.log("ATLETAS_DELETE err="+JSON.stringify(err));
+      console.log("TIPO_EVENTOS_DELETE err="+JSON.stringify(err));
       return reply({statusCode:600});
     }
     return reply({statusCode: 200});
