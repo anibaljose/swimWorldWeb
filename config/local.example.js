7def240845d3c417aedd66ef702cb05d3dd094d6
David Yzaguirre <dvdyzag@gmail.com>
2016-03-05 15:34:17 -0600

    Nuevo copyright y notificación de licencia para código fuente del backend

--- config/local.example.js
+++ config/local.example.js
@@ -1,4 +1,23 @@
-var fs = require('fs');
+/**
+ * @author David Yzaguirre <dvdyzag@gmail.com>
+ *
+ * @copyright Copyright (c) 2016, David Yzaguirre
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+ var fs = require('fs');
 module.exports = {
   auth: 'token', // values: false, 'token'
   host: '0.0.0.0', // A domain name or IP address
